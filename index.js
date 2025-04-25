4c9603a4e3eab5bead4fffbe8bae4a65f16feaa7
rodurico <ploissken@gmail.com>
2025-04-25 18:56:55 -0300

    first commit - express, eslint, nodemon and swisseph

---
+++ index.js
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+import app from "./src/app.js";
+
+dotenv.config();
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Mercur☿ou Server listening on port ${port}`);
+});
