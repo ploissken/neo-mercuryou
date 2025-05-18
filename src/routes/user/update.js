85b4414696be2b7e89872bca267af04f00246f19
rodurico <ploissken@gmail.com>
2025-05-18 11:44:40 -0300

    wip user update

--- src/routes/user/update.js
+++ src/routes/user/update.js
@@ -1,8 +1,4 @@
 import { Router } from "express";
-import bcrypt from "bcryptjs";
-import { insertUser } from "../../db/insert-user.js";
-import { PG_CONSTRAINT_VIOLATION_CODE } from "../../utils/consts.d.js";
-import { generateToken } from "../../utils/jwt.js";
 import { authMiddleware } from "../../utils/auth-middleware.js";
 import { updateUser } from "../../db/update-user.js";
 
@@ -23,28 +19,6 @@ router.post("/update", authMiddleware, async (req, res) => {
   });
 
   return res.send({ ok: true, data: user });
-
-  //   try {
-  //     const user = await insertUser({ email, hash });
-  //     const token = generateToken(user);
-
-  //     res.cookie("token", token, {
-  //       httpOnly: true,
-  //       secure: process.env.NODE_ENV === "production",
-  //       sameSite: "Lax",
-  //       path: "/",
-  //     });
-
-  //     return res.send({ ok: true, data: { user } });
-  //   } catch (err) {
-  //     if (err.code === PG_CONSTRAINT_VIOLATION_CODE) {
-  //       console.error(`[ERROR ${err.code}]: ${email} ${err.message}`);
-  //       return res.send({ ok: false, error: err.constraint });
-  //     } else {
-  //       console.error(`[ERROR ${err.code}]: ${err.message}`);
-  //       return res.send({ ok: false, error: err.code });
-  //     }
-  //   }
 });
 
 export default router;
